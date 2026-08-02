ead50c200c051d8891ed16e2d9df5b284c970627
Swassyman <swassyman2005@gmail.com>
2026-08-02 13:11:22 +0530

    chore: format fix and getter/setter for passHash

--- src/main/java/com/filmr/user/model/User.java
+++ src/main/java/com/filmr/user/model/User.java
@@ -10,7 +10,6 @@ public class User {
   Long id;
 
   String name;
-  String handle;
   String email;
   String passHash;
 
@@ -30,19 +29,19 @@ public class User {
     this.name = name;
   }
 
-  public String getHandle() {
-    return handle;
-  }
-
-  public void setHandle(String handle) {
-    this.handle = handle;
-  }
-
   public String getEmail() {
     return email;
   }
 
   public void setEmail(String email) {
     this.email = email;
+  }
+
+  public String getPassHash() {
+    return passHash;
+  }
+
+  public void setPassHash(String passHash) {
+    this.passHash = passHash;
   }
 }

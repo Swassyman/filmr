149fd1424ff576e021086895ebfde9b48871eee0
Swassyman <swassyman2005@gmail.com>
2026-08-02 13:25:31 +0530

    rename application name and ignore properties

--- src/main/java/com/filmr/FilmrApplication.java
+++ src/main/java/com/filmr/FilmrApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class FilmrApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FilmrApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(FilmrApplication.class, args);
+  }
 }

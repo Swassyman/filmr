560d53e2e5c6e16fccc8436e4d52ddbea14da64b
Swassyman <swassyman2005@gmail.com>
2026-08-04 01:44:18 +0530

    fix: remove unused variables

--- src/main/java/com/filmr/libraryentry/controller/LibraryEntryController.java
+++ src/main/java/com/filmr/libraryentry/controller/LibraryEntryController.java
@@ -2,45 +2,45 @@ package com.filmr.libraryentry.controller;
 
 import com.filmr.libraryentry.model.LibraryEntry;
 import com.filmr.libraryentry.service.LibraryEntryService;
-import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/library")
 public class LibraryEntryController {
 
-  private final LibraryEntryService libraryEntryService;
+	private final LibraryEntryService libraryEntryService;
 
-  LibraryEntryController(LibraryEntryService libraryEntryService) {
-    this.libraryEntryService = libraryEntryService;
-  }
+	LibraryEntryController(LibraryEntryService libraryEntryService) {
+		this.libraryEntryService = libraryEntryService;
+	}
 
-  @GetMapping("/{id}")
-  public ResponseEntity<List<LibraryEntry>> getLibraryEntries(@PathVariable Long userId) {
-    return ResponseEntity.ok(libraryEntryService.getLibraryEntries(userId));
-  }
+	@GetMapping("/{id}")
+	public ResponseEntity<List<LibraryEntry>> getLibraryEntries(@PathVariable Long userId) {
+		return ResponseEntity.ok(libraryEntryService.getLibraryEntries(userId));
+	}
 
-  @GetMapping("/entry/{id}")
-  public ResponseEntity<LibraryEntry> getLibraryEntryById(@PathVariable Long id) {
-    return ResponseEntity.ok(libraryEntryService.getLibraryEntryById(id));
-  }
+	@GetMapping("/entry/{id}")
+	public ResponseEntity<LibraryEntry> getLibraryEntryById(@PathVariable Long id) {
+		return ResponseEntity.ok(libraryEntryService.getLibraryEntryById(id));
+	}
 
-  @PostMapping("/entry/{id}")
-  public ResponseEntity<LibraryEntry> addLibraryEntry(
-      @PathVariable Long id, @RequestBody LibraryEntry entry) {
-    return ResponseEntity.ok(libraryEntryService.addLibraryEntry(entry));
-  }
+	@PostMapping("/entry")
+	public ResponseEntity<LibraryEntry> addLibraryEntry(@RequestBody LibraryEntry entry) {
+		return ResponseEntity.ok(libraryEntryService.addLibraryEntry(entry));
+	}
 
-  @PatchMapping("/entry/{id}")
-  public ResponseEntity<LibraryEntry> updateLibraryEntry(
-      @PathVariable Long id, @RequestBody LibraryEntry entry) {
-    return ResponseEntity.accepted().body(libraryEntryService.updateLibraryEntry(id, entry));
-  }
+	@PatchMapping("/entry/{id}")
+	public ResponseEntity<LibraryEntry> updateLibraryEntry(
+			@PathVariable Long id, @RequestBody LibraryEntry entry) {
+		return ResponseEntity.accepted().body(libraryEntryService.updateLibraryEntry(id, entry));
+	}
 
-  @DeleteMapping("/entry/{id}")
-  public ResponseEntity<Void> deleteLibraryEntry(@PathVariable Long id) {
-    libraryEntryService.deleteLibraryEntry(id);
-    return ResponseEntity.noContent().build();
-  }
+	@DeleteMapping("/entry/{id}")
+	public ResponseEntity<Void> deleteLibraryEntry(@PathVariable Long id) {
+		libraryEntryService.deleteLibraryEntry(id);
+		return ResponseEntity.noContent().build();
+	}
 }

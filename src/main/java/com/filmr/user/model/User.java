1eba2a7889fbf59d607b6e309c131ad09ad579e5
Swassyman <swassyman2005@gmail.com>
2026-08-04 02:15:54 +0530

    fix: remove unused functions

--- src/main/java/com/filmr/user/model/User.java
+++ src/main/java/com/filmr/user/model/User.java
@@ -6,48 +6,40 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "users")
 public class User extends AuditableEntity {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-  @Column(nullable = false)
-  private String name;
+	@Column(nullable = false)
+	private String name;
 
-  @Column(nullable = false, updatable = false, unique = true)
-  private String email;
+	@Column(nullable = false, updatable = false, unique = true)
+	private String email;
 
-  @Column(nullable = false)
-  private String passHash;
+	@Column(nullable = false)
+	private String passHash;
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public String getEmail() {
+		return email;
+	}
 
-  public String getEmail() {
-    return email;
-  }
+	public String getPassHash() {
+		return passHash;
+	}
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassHash() {
-    return passHash;
-  }
-
-  public void setPassHash(String passHash) {
-    this.passHash = passHash;
-  }
+	public void setPassHash(String passHash) {
+		this.passHash = passHash;
+	}
 }
